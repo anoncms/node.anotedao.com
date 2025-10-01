fcf8d071e4be061899e8846a5dd60f153cdb7ccc
Pragmatic <cryptopragmatic@protonmail.com>
2025-10-01 17:56:57 +0200

    Updates

--- app.ts
+++ app.ts
@@ -55,11 +55,11 @@ const start = async () => {
 
         const { chainId } = await provider.getNetwork();
         console.log(chainId);
-        if (chainId != 56) {
-        // if (chainId != 560048) {
+        // if (chainId != 56) {
+        if (chainId != 560048) {
             await window.ethereum.request({
                 method: "wallet_addEthereumChain",
-                params: params
+                params: paramsTestnet
             });
             window.location.href = "./";
         }
